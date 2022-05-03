338d579a2d91ebdea818973c2e04ab9c19d72f78
Kelly N. Bodwin <kelly@bodwin.us>
2022-05-03 12:12:17 -0700

    qmds

---
+++ installs.R
@@ -0,0 +1,10 @@
+system("python3 -m pip install jupyter")
+system("python3 -m pip install seaborn")
+system("python3 -m pip install pandas")
+system("python3 -m pip install numpy")
+system("python3 -m pip install sklearn")
+system("python3 -m pip install statsmodels")
+
+
+## For use outside RStudio Cloud:
+# py_install(c("jupyter", "seaborn", "pandas", "numpy", "sklearn", "statsmodels"))
